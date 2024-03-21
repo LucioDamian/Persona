234a26848f392115d30ea314fda182dacfed6189
LucioDamian <damianplus015@gmail.com>
2024-03-21 10:42:46 +0100

    Agregando archivos a la carpeta Persona

---
+++ src/modelo/Model/Persona.java
@@ -0,0 +1,62 @@
+package modelo.Model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String DNI;
+
+    /**
+     * 
+     */
+    private String Nombre;
+
+    /**
+     * 
+     */
+    private String Apellido1;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellido1, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+}
